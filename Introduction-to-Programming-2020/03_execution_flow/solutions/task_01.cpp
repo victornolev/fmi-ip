2806b943cc80710b739a916bd1e76dd667f7f07c
Victor Nolev <victornolev@gmail.com>
2021-01-03 19:52:05 +0200

    Create task_01.cpp

---
+++ Introduction-to-Programming-2020/03_execution_flow/solutions/task_01.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file   task_01.cpp
+ * @author Victor Nolev
+ * @date   21.11.2020
+ * @brief  Solutions for Task 1 from Practice 3.
+ * Requirements: Solve task without using loops and further knowledge.
+ */
+
+#include <iostream>
+
+int main() {
+    int firstNumber;
+    int secondNumber;
+    std::cin >> firstNumber >> secondNumber;
+    std::cout << "Bigger number is: "
+              << ((firstNumber >= secondNumber) ? firstNumber : secondNumber)
+              << std::endl;
+    return 0;
+}
